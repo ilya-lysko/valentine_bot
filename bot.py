ac164f952d890579952a617745636fbc2dc46441
kinetik <kinetikm@gmail.com>
2018-01-13 17:37:11 +0300

    change bot py

--- bot.py
+++ bot.py
@@ -1,56 +1,67 @@
-import config
-import state
+# -*- coding: utf-8 -*-
+
 import telebot
+import config
+import dbworker
 
 bot = telebot.TeleBot(config.token)
-current_state = state.States.S_ENTER_NAME.value
-markup = telebot.types.ReplyKeyboardMarkup()
 
-@bot.message_handler(commands=['start'])
-def start(message):
-	"""
-	Регистрация нового пользователя, после нажатия кнопки "Start"
-	"""
-	current_state = state.States.S_START
-	bot.send_message(message.chat.id, 'Привет, я бот-купидон для мифистов.\nКак тебя зовут?')
-	current_state = state.States.S_ENTER_NAME.value
-	
-@bot.message_handler(func=lambda message: current_state == state.States.S_ENTER_NAME.value)
-def change_name(message):
-    # пока просто перешлем пользователю его же имя
-    bot.send_message(message.chat.id, message.text)
-    bot.send_message(message.chat.id, "Отличное имя, запомнил! Теперь укажи, пожалуйста, свой пол.")
-    current_state = state.States.S_ENTER_SEX.value
 
-@bot.message_handler(func=lambda message: current_state == state.States.S_ENTER_SEX.value)
-def change_sex(message):
-    current_state = state.States.S_ENTER_SEX.value
-    markup.row('муж.', 'жен.')
-    if message.text in ['муж.','жен.']:
-    	# пока просто перешлем пол пользователю
-    	bot.send_message(message.chat.id, message.text)
-    	markup = telebot.types.ReplyKeyboardMarkup()
-    	current_state = state.States.S_ENTER_AGE.value
+# Начало диалога
+@bot.message_handler(commands=["start"])
+def cmd_start(message):
+    state = dbworker.get_current_state(message.chat.id)
+    if state == config.States.S_ENTER_NAME.value:
+        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить своё имя, но так и не сделал этого :( Жду...")
+    elif state == config.States.S_ENTER_AGE.value:
+        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить свой возраст, но так и не сделал этого :( Жду...")
+    elif state == config.States.S_SEND_PIC.value:
+        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить картинку, но так и не сделал этого :( Жду...")
+    else:  # Под "остальным" понимаем состояние "0" - начало диалога
+        bot.send_message(message.chat.id, "Привет! Как я могу к тебе обращаться?")
+        dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
+
+
+# По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
+@bot.message_handler(commands=["reset"])
+def cmd_reset(message):
+    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Как тебя зовут?")
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
+
+
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_NAME.value)
+def user_entering_name(message):
+    # В случае с именем не будем ничего проверять, пусть хоть "25671", хоть Евкакий
+    bot.send_message(message.chat.id, "Отличное имя, запомню! Теперь укажи, пожалуйста, свой возраст.")
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_AGE.value)
+
+
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_AGE.value)
+def user_entering_age(message):
+    # А вот тут сделаем проверку
+    if not message.text.isdigit():
+        # Состояние не меняем, поэтому только выводим сообщение об ошибке и ждём дальше
+        bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
+        return
+    # На данном этапе мы уверены, что message.text можно преобразовать в число, поэтому ничем не рискуем
+    if int(message.text) < 5 or int(message.text) > 100:
+        bot.send_message(message.chat.id, "Какой-то странный возраст. Не верю! Отвечай честно.")
+        return
     else:
-        pass
-        # тут нужно вообще что-то?
+        # Возраст введён корректно, можно идти дальше
+        bot.send_message(message.chat.id, "Когда-то и мне было столько лет...эх... Впрочем, не будем отвлекаться. "
+                                          "Отправь мне какую-нибудь фотографию.")
+        dbworker.set_state(message.chat.id, config.States.S_SEND_PIC.value)
 
-@bot.message_handler(func=lambda message: current_state == state.States.S_ENTER_AGE.value)
-def change_age(message):
-    if message.text.isdigit() and 30 > int(message.text) >= 16:
-        # пока просто перешлем пользователю его возраст
-        bot.send_message(message.chat.id, message.text)
-        bot.send_message(message.chat.id, 'Когда будешь готов, введи команду /help.')
-        current_state = state.States.S_SEND_USER.value
-    else:
-        bot.send_message(message.chat.id, 'Возраст должен быть числом от 16 до 29 (включительно). Давай еще раз.')
 
-@bot.message_handler(commands=['game'])
-def game(message):
-	"""
-	Сам процесс подбора партнеров и сбора ответов
-	"""
-	bot.send_message(message.chat.id, 'Игра', reply_markup=markup)
+@bot.message_handler(content_types=["photo"],
+                     func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_SEND_PIC.value)
+def user_sending_photo(message):
+    # То, что это фотография, мы уже проверили в хэндлере, никаких дополнительных действий не нужно.
+    bot.send_message(message.chat.id, "Отлично! Больше от тебя ничего не требуется. Если захочешь пообщаться снова - "
+                     "отправь команду /start.")
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bot.polling(none_stop=True)
