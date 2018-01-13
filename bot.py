74ae243b7bd649ce7607fd51497d17c71fd0f84c
ilysko <ilya.lysko@gmail.com>
2018-01-13 15:04:20 +0300

    Yes\No buttons

--- bot.py
+++ bot.py
@@ -3,9 +3,12 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+markup = telebot.types.ReplyKeyboardMarkup()
+markup.row('Yes', 'No')
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_msg(message):
-    bot.send_message(message.chat.id, message.text)
+def make_choice(message):
+    bot.send_message(message.chat.id, 'Make your choice:', reply_markup=markup)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
